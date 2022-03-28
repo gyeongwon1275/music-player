88a38751766d19d9838d1df8a670e10a3ae0b1e7
gyeongwon1275 <rlaruddnjs28@gmail.com>
2022-03-28 15:01:36 +0900

    Refactor: Intro 익명 class 로 변환
    - customElement define 할때 이름 붙이므로 class 이름이 굳이 필요없음

--- src/Components/Intro.ts
+++ src/Components/Intro.ts
@@ -1,41 +1,33 @@
-class Intro extends HTMLElement {
-  private _time: number;
-
-  constructor() {
-    super();
-
-    this._time = Number(this.getAttribute('time'));
-    this.open = true;
-  }
-
-  get open() {
-    return this.hasAttribute('open');
-  }
-
-  set open(val) {
-    if (val) {
-      this.setAttribute('open', '');
-    } else {
-      this.removeAttribute('open');
-    }
-  }
-
-  connectedCallback() {
-    setTimeout(() => {
-      this.open = false;
-    }, this._time || 3000);
-  }
-}
-
-declare global {
-  interface Window {
-    Intro: typeof Intro;
-  }
-}
-
-export default Intro;
-
 if (!window.customElements.get('x-intro')) {
-  window.customElements.define('x-intro', Intro);
-  window.Intro = Intro;
+  window.customElements.define(
+    'x-intro',
+    class extends HTMLElement {
+      private _time: number;
+
+      constructor() {
+        super();
+
+        this._time = Number(this.getAttribute('time'));
+        this.open = true;
+      }
+
+      get open() {
+        return this.hasAttribute('open');
+      }
+
+      set open(val) {
+        if (val) {
+          this.setAttribute('open', '');
+        } else {
+          this.removeAttribute('open');
+        }
+      }
+
+      connectedCallback() {
+        setTimeout(() => {
+          this.open = false;
+        }, this._time || 3000);
+      }
+    }
+  );
 }
